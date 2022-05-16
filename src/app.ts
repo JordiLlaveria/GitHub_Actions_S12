96dd0901fa4c3eebe28f69adc9c448de51fe01ce
JordiLlaveria <jordi.llaveria@estudiantat.upc.edu>
2022-05-16 22:06:50 +0200

    app

--- src/app.ts
+++ src/app.ts
@@ -61,5 +61,15 @@ class App {
   }
 }
 
+let suma: number = 0;
+
+let sumacorrecte:boolean = false
+
+suma=3+3;
+    suma = suma +suma;
+
+if (suma==9)
+    sumacorrecte=true;
+
 export default new App().app;
 
